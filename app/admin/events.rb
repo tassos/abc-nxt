9261770b73acd9fc93c2b749d711d9d0be9093f2
Tassos (nakano) Natsakis <891234ser@eipieq.com>
2018-03-03 20:27:36 +0200

    White list form params

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Event do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :name, :start_date, :end_date
+permit_params :name, :start_date, :end_date, :status
 
 # or
 #
