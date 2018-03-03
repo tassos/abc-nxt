b4e3bae681fe29af26ab45ed143cd273dd86a97a
Tassos (nakano) Natsakis <891234ser@eipieq.com>
2018-03-03 20:28:34 +0200

    Add status for an event

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
   has_many :applications
   has_many :users, through: :applications
   has_many :questions
+
+  enum status: [:open, :closed, :cancelled, :announced]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180225133947) do
+ActiveRecord::Schema.define(version: 20180225140155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20180225133947) do
     t.datetime "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status"
   end
 
 # Could not dump table "pri_keys" because of following StandardError
